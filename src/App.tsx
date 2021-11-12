91b80c6080d243806be4ad8948286c71ec220144
Arthur Samuel <arthur.samuel@devstack.com.au>
2021-11-12 19:48:16 +0800

    add auth before enter

--- src/App.tsx
+++ src/App.tsx
@@ -18,25 +18,45 @@ import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Main from "./components/layout/Main";
-import PasswordReset from './components/sdsb-component/password_reset/PasswordReset'
+import PasswordReset from "./components/sdsb-component/password_reset/PasswordReset";
 import "antd/dist/antd.css";
 import "./assets/styles/main.css";
 import "./assets/styles/responsive.css";
+import { useState } from "react";
+
+interface IPop {
+  component: JSX.Element;
+  path: string;
+}
 
 function App() {
+  const [token,] = useState(true);
+
+  const CreateRoute = function (props: IPop) {
+    return (
+      <Route exact path={props.path}>
+        {token && props.component}
+        {!token && <Redirect to="/sign-up" />}
+      </Route>
+    );
+  };
+
   return (
     <div className="App">
       <Switch>
         <Route path="/sign-up" exact component={SignUp} />
         <Route path="/sign-in" exact component={SignIn} />
         <Main>
-          <Route exact path="/dashboard" component={Home} />
-          <Route exact path="/tables" component={Tables} />
-          <Route exact path="/billing" component={Billing} />
-          <Route exact path="/rtl" component={Rtl} />
-          <Route exact path="/profile" component={Profile} />
-          <Route exact path="/password-reset" component={PasswordReset} />
-          <Redirect from="*" to="/dashboard" />
+          <Route exact path="/">
+            {token && <Redirect to="/dashboard" />}
+            {!token && <Redirect to="/sign-up" />}
+          </Route>
+          <CreateRoute component={<Home />} path="/dashboard"></CreateRoute>
+          <CreateRoute component={<Tables />} path="/tables"></CreateRoute>
+          <CreateRoute component={<Billing />} path="/billing"></CreateRoute>
+          <CreateRoute component={<Rtl />} path="/rtl"></CreateRoute>
+          <CreateRoute component={<Profile />} path="/profile"></CreateRoute>
+          <Route exact component={PasswordReset} path="/password-reset" />
         </Main>
       </Switch>
     </div>

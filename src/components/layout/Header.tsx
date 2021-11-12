6f131baea0bfe8daae69fa3389320d2f8cc7be88
Arthur Samuel <arthur.samuel@devstack.com.au>
2021-11-13 02:48:03 +0800

    remove notification

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,62 +1,6 @@
 import { useState, useEffect, Fragment } from "react";
-import { Row, Col, Badge, Dropdown, Button, List, Avatar } from "antd";
-import {
-  FacebookFilled,
-  IdcardOutlined,
-  BellOutlined,
-  ClockCircleFilled,
-} from "@ant-design/icons";
-
-const data = [
-  {
-    title: "New message from Sophie",
-    description: (
-      <>
-        {" "}
-        <ClockCircleFilled></ClockCircleFilled> 2 days ago
-      </>
-    ),
-    avatar: <FacebookFilled></FacebookFilled>,
-  },
-  {
-    title: "New album by Travis Scott",
-    description: (
-      <>
-        {" "}
-        <ClockCircleFilled></ClockCircleFilled> 2 days ago
-      </>
-    ),
-    avatar: <FacebookFilled></FacebookFilled>,
-  },
-  {
-    title: "Payment completed",
-    description: (
-      <>
-        {" "}
-        <ClockCircleFilled></ClockCircleFilled> 2 days ago
-      </>
-    ),
-    avatar: <FacebookFilled></FacebookFilled>,
-  },
-];
-
-const menu = (
-  <List
-    min-width="100%"
-    className="header-notifications-dropdown "
-    itemLayout="horizontal"
-    dataSource={data}
-    renderItem={(item) => (
-      <List.Item>
-        <List.Item.Meta
-          avatar={<Avatar shape="square" src={item.avatar} />}
-          title={item.title}
-          description={item.description}
-        />
-      </List.Item>
-    )}
-  />
-);
+import { Row, Col, Button } from "antd";
+import { IdcardOutlined } from "@ant-design/icons";
 
 const toggler = [
   <svg
@@ -88,17 +32,6 @@ function Header(props: IHeader) {
       <Row gutter={[24, 0]}>
         <Col span={24} md={6}></Col>
         <Col span={24} md={18} className="header-control">
-          <Badge size="small" count={4}>
-            <Dropdown overlay={menu} trigger={["click"]}>
-              <a
-                href="#pablo"
-                className="ant-dropdown-link"
-                onClick={(e) => e.preventDefault()}
-              >
-                <BellOutlined></BellOutlined>
-              </a>
-            </Dropdown>
-          </Badge>
           <Button
             type="link"
             className="sidebar-toggler"
@@ -110,7 +43,6 @@ function Header(props: IHeader) {
             style={{
               display: "flex",
               alignItems: "center",
-              marginRight: 20,
               border: "1px solid #dedede",
               padding: 10,
               borderRadius: 5,

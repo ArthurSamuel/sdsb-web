2c5807a53df8e0b856bdfd02333c3e0b4229b42c
Arthur Samuel <arthur.samuel@devstack.com.au>
2021-11-13 02:33:06 +0800

    update table

---
+++ src/pages/Tables.tsx
@@ -0,0 +1,62 @@
+import { Card, Button, Tabs, Row, Col } from "antd";
+import { Fragment, useState } from "react";
+import "../assets/styles/billing.css";
+
+function Tables() {
+  const { TabPane } = Tabs;
+  const [showQr, setShowQr] = useState(false);
+  const QrReader = require("react-qr-reader");
+  const QRCodeGenerate = require("qrcode.react");
+
+  return (
+    <Fragment>
+      <Tabs>
+        <TabPane tab="QR Code Saya" key="1">
+          <div className="container-billing">
+            <span className="title">
+              Scan QR Code untuk otomatis dialihkan ke halaman tranfer SDSB
+            </span>
+          </div>
+          <div className="container-billing">
+            <Card style={{ marginTop: 20 }}>
+              <QRCodeGenerate
+                size={250}
+                value="https://www.google.com"
+              ></QRCodeGenerate>
+            </Card>
+          </div>
+        </TabPane>
+        <TabPane tab="Scan QR Code" key="2">
+          <div className="container-billing">
+            <Card style={{ marginTop: 20 }}>
+              <div style={{ marginBottom: 20 }}>
+                <span className="title">
+                  Mulai Scan QR Code Untuk Melakukan Transaksi
+                </span>
+              </div>
+              <Button
+                style={{ width: "100%" }}
+                type="primary"
+                onClick={() => setShowQr((prev) => !prev)}
+              >
+                {showQr ? "Berhenti" : "Mulai"}
+              </Button>
+            </Card>
+            {showQr && (
+              <div style={{ marginTop: 20, width: "100%" }}>
+                <QrReader
+                  style={{ width: "100%" }}
+                  onScan={(e: any) => console.log(e)}
+                  delay={300}
+                  onError={() => console.log("asd")}
+                ></QrReader>
+              </div>
+            )}
+          </div>
+        </TabPane>
+      </Tabs>
+    </Fragment>
+  );
+}
+
+export default Tables;

2818888c9fa6d9f5254939b0fd43f962be60605b
Arthur Samuel <arthur.samuel@devstack.com.au>
2021-11-18 14:13:48 +0800

    update route

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,18 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 import { Switch, Route, Redirect } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
 import Tables from "./pages/QrcodeComponent";
 import Billing from "./pages/Billing";
 import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
-import SignIn from "./pages/SignIn";
+import SignIn from "./pages/Sign_in/SignIn";
 import Main from "./components/layout/Main";
 import PasswordReset from "./components/sdsb-component/password_reset/PasswordReset";
 import "antd/dist/antd.css";
 import "./assets/styles/main.css";
 import "./assets/styles/responsive.css";
-import { useState } from "react";
+import { KeyToken } from './utils/Constant'
 
 interface IPop {
   component: JSX.Element;
@@ -29,13 +29,13 @@ interface IPop {
 }
 
 function App() {
-  const [token,] = useState(true);
+  const token = localStorage.getItem(KeyToken) ? true : false
 
   const CreateRoute = function (props: IPop) {
     return (
       <Route exact path={props.path}>
         {token && props.component}
-        {!token && <Redirect to="/sign-up" />}
+        {!token && <Redirect to="/sign-in" />}
       </Route>
     );
   };
@@ -44,7 +44,10 @@ function App() {
     <div className="App">
       <Switch>
         <Route path="/sign-up" exact component={SignUp} />
-        <Route path="/sign-in" exact component={SignIn} />
+        <Route path="/sign-in" exact  >
+          {!token && <SignIn />}
+          {token && <Redirect to="/dashboard" />}
+        </Route>
         <Main>
           <Route exact path="/">
             {token && <Redirect to="/dashboard" />}

9df908eb0ea9b156286d8bdb4f194e874a1a6490
Arthur Samuel <arthur.samuel@devstack.com.au>
2021-11-19 01:45:11 +0800

    update profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,11 +1,22 @@
-import { Fragment, useState } from "react";
-import { Row, Col } from "antd";
+import { Fragment, useEffect, useState } from "react";
+import { Row, Col, Modal } from "antd";
 import BgProfile from "../assets/images/bg-profile.jpg";
 import profilavatar from "../assets/images/face-1.jpg";
 import ProfileComponent from "../components/sdsb-component/profile/Profile";
 import ProfilePicture from "../components/sdsb-component/profile_picture/ProfilePicture";
+import PasswordReset from "../components/sdsb-component/password_reset/PasswordReset";
+import PinReset from "../components/sdsb-component/pin_reset/PinReset";
 
 function Profile() {
+  const [showModal, setShowModal] = useState(false);
+  const [credential, setCredential] = useState<"PIN" | "Password" | null>(null);
+
+  useEffect(() => {
+    if (credential) {
+      setShowModal(true);
+    }
+  }, [credential]);
+
   return (
     <Fragment>
       <ProfilePicture
@@ -16,12 +27,28 @@ function Profile() {
       <Row gutter={[24, 0]}>
         <Col span={24} md={24} lg={24} className="mb-24">
           <ProfileComponent
+            onChangePassword={() => setCredential("Password")}
+            onChangePIN={() => setCredential("PIN")}
             memberCode="CUST_A123"
             name="Arthur Samuel"
             username="Arth"
           ></ProfileComponent>
         </Col>
       </Row>
+      <Modal
+        visible={showModal}
+        footer={null}
+        onCancel={() => {
+          setShowModal(false);
+          setCredential(null);
+        }}
+      >
+        {credential === "PIN" ? (
+          <PinReset></PinReset>
+        ) : (
+          <PasswordReset></PasswordReset>
+        )}
+      </Modal>
     </Fragment>
   );
 }

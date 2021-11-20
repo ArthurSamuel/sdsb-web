e5935dc7cb270d07db4de391521ce617316d4c64
Arthur Samuel <arthur.samuel@devstack.com.au>
2021-11-20 16:49:06 +0800

    update constant

--- src/utils/Constant.ts
+++ src/utils/Constant.ts
@@ -2,5 +2,12 @@ export const KeyToken = 'key'
 
 export const Server = {
   dev: 'http://159.223.50.120/api',
-  prod: 'http://159.223.50.120/api'
+  prod: 'http://159.223.50.120/api',
+  baseProd: 'http://159.223.50.120'
 }
+
+export const CustomerServicePhone = '1234'
+
+export const CustomerServiceWA = '1234'
+
+export const CustomerServiceWAMessage = 'Saya Butuh Bantuan untuk...'
